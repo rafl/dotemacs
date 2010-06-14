fd076ef521d3f9af02c1d0fe74ab024c57a2c880
Florian Ragwitz <rafl@debian.org>
2010-06-14 18:28:18 +0200

    Use ibuffer
    also do proper commenting and turn off trailing whitespace
    highlighting for Help

--- 02-interface.el
+++ 02-interface.el
@@ -1,55 +1,60 @@
- ; disable some ui components
+;; disable some ui components
 (menu-bar-mode 0)
 (tool-bar-mode 0)
 (scroll-bar-mode -1)
 
-; stop cursor from blinking
+;; stop cursor from blinking
 (blink-cursor-mode 0)
 
-; no welcome message
+;; no welcome message
 (setq inhibit-startup-message t)
 
-; no echo area help on startup
+;; no echo area help on startup
 (defun display-startup-echo-area-message ()
   (message ""))
 
-; display position in modeline
+;; display position in modeline
 (setq line-number-mode t)
 (setq column-number-mode t)
 
-; syntax highlighting everywhere
+;; syntax highlighting everywhere
 (global-font-lock-mode t)
 
-; show matching parnens (mixed style)
+;; show matching parnens (mixed style)
 (show-paren-mode t)
 (setq show-paren-delay 0.0)
 (setq show-paren-style 'parenthesis)
 
-; hilight selection
+;; hilight selection
 (transient-mark-mode t)
 
-; make "yes or no" prompts show "y or n" instead
+;; make "yes or no" prompts show "y or n" instead
 (fset 'yes-or-no-p 'y-or-n-p)
 
-; custom colors
+;; custom colors
 (set-background-color "white")
 (set-foreground-color "black")
 
-; switching
+;; switching
 (iswitchb-mode 1)
 (icomplete-mode 1)
 
-; no backup files
+;; no backup files
 (setq make-backup-files nil)
 (setq auto-save-default nil)
 
-; case insensitive completion
+;; case insensitive completion
 (setq completion-ignore-case t)
 
-; always indent new lines by default
+;; always indent new lines by default
 (global-set-key "\r" 'newline-and-indent)
+(global-set-key (kbd "C-x C-b") 'ibuffer)
 
 (setq require-final-newline 'visit-save)
+
+(add-hook 'help-mode-hook
+          (lambda ()
+            (setq show-trailing-whitespace nil)))
 
 (setq fill-column 80)
 (highlight-beyond-fill-column)
