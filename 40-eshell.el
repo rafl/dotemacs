2e81d88473695401f284745262ae95566ff3ecc0
Florian Ragwitz <rafl@debian.org>
2010-06-14 18:25:49 +0200

    Start configuring eshell

---
+++ 40-eshell.el
@@ -0,0 +1,24 @@
+(require 'eshell)
+
+(add-hook 'eshell-mode-hook
+          (lambda ()
+            (setq show-trailing-whitespace nil)
+            (eldoc-mode)))
+
+(setq
+ eshell-buffer-name "*shell*"
+ eshell-banner-message ""
+ eshell-cmpl-autolist t
+ eshell-cmpl-restore-window-delay 0
+ eshell-error-if-no-glob t
+ eshell-history-size 3000
+ eshell-hist-ignoredups t
+ eshell-visual-commands
+ '("vi" "vim" "screen" "top" "htop" "less" "more" "lynx"
+   "ncftp" "pine" "tin" "trn" "elm" "iftop" "jnettop"
+   "mplayer"))
+
+;; TODO: make visual-commands use ansi-term instead of term
+
+(defun eshell/perldoc (&rest args)
+  (perldoc (apply 'eshell-flatten-and-stringify args)))
