a76338e8bbdeba55ae883233c443d319bf42b60c
Florian Ragwitz <rafl@debian.org>
2010-06-14 18:27:35 +0200

    Load and bind eproject-extras

--- 40-eproject.el
+++ 40-eproject.el
@@ -1,4 +1,5 @@
 (require 'eproject)
+(require 'eproject-extras)
 
 (define-project-type perl (generic)
   (or (look-for "dist.ini") (look-for "Makefile.PL") (look-for "Build.PL"))
@@ -13,3 +14,20 @@
                               m))
                            (t file))))
   :main-file "dist.ini")
+
+(defmacro .emacs-curry (function &rest args)
+  `(lambda () (interactive)
+     (,function ,@args)))
+
+(defmacro .emacs-eproject-key (key command ep-only)
+  (cons 'progn
+        (loop for (k . p) in (list (cons key 4) (cons (upcase key) 1))
+              collect
+              `(,@(if ep-only '(define-key eproject-mode-map) '(global-set-key))
+                (kbd ,(format "C-x p %s" k))
+                (.emacs-curry ,command ,p)))))
+
+(.emacs-eproject-key "k" eproject-kill-project-buffers t)
+(.emacs-eproject-key "v" eproject-revisit-project nil)
+(.emacs-eproject-key "b" eproject-ibuffer t)
+(.emacs-eproject-key "o" eproject-open-all-project-files t)
