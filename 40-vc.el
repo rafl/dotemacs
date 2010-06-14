163a41dce4ffdb1c39f00a062c597847e43eccac
Florian Ragwitz <rafl@debian.org>
2010-06-14 23:04:59 +0200

    Update TODO

--- 40-vc.el
+++ 40-vc.el
@@ -1,1 +1,4 @@
+;; this basically disables vc.el completely.
+;; it's not too useful, and, when started automatically, often gets in
+;; the way of things by taking ages to initialize
 (setq vc-handled-backends nil)
