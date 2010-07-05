b78e2a9d719d1b23cc00b0b461eeee7b585c98a2
Florian Ragwitz <rafl@debian.org>
2010-07-05 04:45:02 +0200

    Add newsticker configuration

---
+++ 40-newsticker.el
@@ -0,0 +1,15 @@
+(require 'newsticker)
+
+(setq
+ newsticker-html-renderer 'w3m-region
+ newsticker-date-face '((t (:slant italic :height 1.0 :family "helvetica")))
+ newsticker-extra-face '((t (:foreground "gray50" :slant italic :height 1.0)))
+ newsticker-feed-face '((t (:foreground "black" :weight bold :height 1.3)))
+ newsticker-new-item-face '((t (:weight bold :family "helvetica"))))
+
+(let ((feeds-file (mapconcat
+                   'identity
+                   (list emacs-conf-path "newsticker-feeds.el")
+                   "/")))
+  (when (not (ignore-errors (load feeds-file)))
+    (message "Failed to load newsticker feeds from %s" feeds-file)))
