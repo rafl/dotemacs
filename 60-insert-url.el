24747cb8764405edf0413063f659429e5a080fbf
Florian Ragwitz <rafl@debian.org>
2010-07-05 07:28:08 +0200

    Add helpers for opening urls

---
+++ 60-insert-url.el
@@ -0,0 +1,20 @@
+(defun insert-url (url)
+  (interactive
+   (list (read-string "URL: ")))
+  (let ((buf (url-retrieve-synchronously url)))
+    (when buf
+      (insert
+       (or (with-current-buffer buf
+             (goto-char (point-min))
+             (when (re-search-forward "\n\n" nil t)
+               (buffer-substring (match-end 0) (point-max))))
+           ""))
+      (kill-buffer buf))))
+
+(defun find-url (url)
+  (interactive
+   (list (read-string "URL: ")))
+  (with-current-buffer (get-buffer-create url)
+    (if (not (insert-url url))
+        (kill-buffer)
+      (pop-to-buffer (current-buffer)))))
