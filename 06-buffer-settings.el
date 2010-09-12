28350bef6c51b46fdfc122ba0d925fe00cc0c750
Florian Ragwitz <rafl@debian.org>
2010-09-12 17:07:30 +0200

    Rename :name to :filename, so :name is available for buffer names

--- 06-buffer-settings.el
+++ 06-buffer-settings.el
@@ -6,7 +6,7 @@
           (:mode org-mode)
           (:mode pod-mode)
           (:mode markdown-mode)
-          (:name "\\.\\(txt\\|mkn\\)$"))
+          (:filename "\\.\\(txt\\|mkn\\)$"))
          (lambda ()
            (flyspell-mode 1)
            (auto-fill-mode 1)))
@@ -27,15 +27,13 @@
 
 (defun rafl:evaluate-buffer-condition (con)
   (cond
-   ((functionp con)
-    (funcall con))
+   ((functionp con) (funcall con))
    ((listp con)
     (cond
      ((listp (car con))
-      (reduce
-       (lambda (a b) (or a b))
-       (mapcar #'rafl:evaluate-buffer-condition con)
-       :initial-value nil))
+      (reduce (lambda (a b) (or a b))
+              (mapcar #'rafl:evaluate-buffer-condition con)
+              :initial-value nil))
      (t
       (reduce
        (lambda (a b) (and a b))
@@ -43,42 +41,35 @@
          (while con
            (let ((k (pop con))
                  (v (pop con)))
-             (push (cond
-                    ((eq k :fun)
-                     (funcall v))
-                    ((eq k :not)
-                     (when (not (listp v))
-                       (error ":not requires a list"))
-                     (not (rafl:evaluate-buffer-condition v)))
-                    ((eq k :mode)
-                     (if (stringp v)
-                         (string-match-p v (symbol-name major-mode))
-                       (eq v major-mode)))
-                    ((eq k :name)
-                     (cond
-                      ((and (buffer-file-name) (stringp v))
+             (push
+              (cond
+               ((eq k :fun) (funcall v))
+               ((eq k :not)
+                (when (not (listp v)) (error ":not requires a list"))
+                (not (rafl:evaluate-buffer-condition v)))
+               ((eq k :mode)
+                (if (stringp v)
+                    (string-match-p v (symbol-name major-mode))
+                  (eq v major-mode)))
+               ((eq k :name)
+                (string-match-p v (buffer-name)))
+               ((eq k :filename)
+                (cond ((and (buffer-file-name) (stringp v))
                        (string-match-p v (buffer-file-name)))
-                      ((buffer-file-name)
-                       v)
-                      (t
-                       (not v))))
-                    ((eq k :project)
-                     ;; assume eproject-maybe-turn-on was called from an earlier
-                     ;; hook
-                     (cond
-                      ((and eproject-root (stringp v))
-                       (string= v (eproject-name)))
-                      (eproject-root
-                       v)
-                      (t
-                       (not v))))
-                    (t
-                     (error "unknown cond")))
-                   ret)))
+                      ((buffer-file-name) v)
+                      (t (not v))))
+               ((eq k :project)
+                ;; assume eproject-maybe-turn-on was called from an earlier hook
+                (cond
+                 ((and eproject-root (stringp v))
+                  (string= v (eproject-name)))
+                 (eproject-root v)
+                 (t (not v))))
+               (t (error "unknown cond")))
+              ret)))
          ret)
        :initial-value t))))
-   (t
-    (error "invalid condition"))))
+   (t (error "invalid condition"))))
 
 (defun rafl:apply-buffer-settings (settings)
   (dolist (setting rafl:buffer-settings)
